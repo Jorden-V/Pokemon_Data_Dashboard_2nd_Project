96c162f3b4a71510c9bc45e46256aa2cde3d6868
Jorden <jordenkv@gmail.com>
2019-03-21 14:58:16 +0000

    Added navbar, footer and basic divs to hold graph elements

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,11 +1,89 @@
 queue()
-    .defer(d3.csv, "data/performance.csv") //add data filepath here
+    .defer(d3.csv, "data/performance.csv")
     .await(makeGraphs);
-    
-function makeGraphs (error, performance) { //add data filename here
-    var ndx = crossfilter(performance); // add same filename as above in here
-    
-    #(ndx); //graph name in place of #
-    
+
+function makeGraphs(error, performance) {
+    var ndx = crossfilter(performance);
+
+    show_discipline_selector(ndx);
+    gender_volume(ndx);
+    race_participation(ndx);
     dc.renderAll();
+}
+
+function show_discipline_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('gender'));
+    var group = dim.group();
+
+    dc.selectMenu("#discipline-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+function gender_volume(ndx) {
+    var dim = ndx.dimension(dc.pluck('gender'));
+    var group = dim.group();
+
+    dc.barChart("#gender-volume")
+        .width(350)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
+}
+
+function race_participation(ndx) {
+
+    function race_participation(dimension, race) {
+        return dimension.group().reduce(
+            function(p, v) {
+                p.total++;
+                if (v.race == race) {
+                    p.match++;
+                }
+                return p;
+            },
+            function(p, v) {
+                p.total--;
+                if (v.race == race) {
+                    p.match--;
+                }
+                return p;
+            },
+            function() {
+                return { total: 0, match: 0 };
+            }
+        );
+    }
+
+    var dim = ndx.dimension(dc.pluck("sex"));
+    var aByGender = raceByGender(dim, "group A");
+    var asstProfByGender = rankByGender(dim, "AsstProf");
+    var assocProfByGender = rankByGender(dim, "AssocProf");
+
+    dc.barChart("#rank-distribution")
+        .width(350)
+        .height(250)
+        .dimension(dim)
+        .group(profByGender, "Prof")
+        .stack(asstProfByGender, "Asst Prof")
+        .stack(assocProfByGender, "Assoc Prof")
+        .valueAccessor(function(d) {
+            if (d.value.total > 0) {
+                return (d.value.match / d.value.total) * 100;
+            }
+            else {
+                return 0;
+            }
+        })
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
+        .margins({ top: 10, right: 100, bottom: 30, left: 30 });
 }
