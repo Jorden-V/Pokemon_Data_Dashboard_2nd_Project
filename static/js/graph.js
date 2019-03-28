4e8b6e382ca021f344c2e1ab2e7da36f477ed3d5
Jorden <jordenkv@gmail.com>
2019-03-28 11:08:33 +0000

    Included number displays for total soliders of all class

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,19 +7,62 @@ function makeGraphs(error, mildata) {
     var ndx = crossfilter(mildata);
     soldiers_by_country(ndx);
     country_selector(ndx);
+    show_total_troops(ndx);
+    show_total_regular_troops(ndx);
+    show_total_reserve_troops(ndx);
+    show_total_paramilitary(ndx);
     dc.renderAll();
+}
+
+function show_total_troops(ndx)  {
+var show_total_troops = ndx.groupAll().reduceSum(dc.pluck('Total'));
+dc.numberDisplay("#total-troops")
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function(d) {
+        return (+d);
+    })
+    .group(show_total_troops);
+}
+
+function show_total_regular_troops(ndx)  {
+var show_total_troops = ndx.groupAll().reduceSum(dc.pluck('Active military'));
+dc.numberDisplay("#regular-troops")
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function(d) {
+        return (+d);
+    })
+    .group(show_total_troops);
+}
+
+function show_total_reserve_troops(ndx)  {
+var show_total_troops = ndx.groupAll().reduceSum(dc.pluck('Reserve military'));
+dc.numberDisplay("#reserve-troops")
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function(d) {
+        return (+d);
+    })
+    .group(show_total_troops);
+}
+
+function show_total_paramilitary(ndx)  {
+var show_total_troops = ndx.groupAll().reduceSum(dc.pluck('Paramilitary'));
+dc.numberDisplay("#paramilitary")
+    .formatNumber(d3.format("d"))
+    .valueAccessor(function(d) {
+        return (+d);
+    })
+    .group(show_total_troops);
 }
 
 
 function country_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('country'));
     var group = dim.group();
-
+    
     dc.selectMenu("#country-selector")
         .dimension(dim)
         .group(group);
 }
-
 
 
 function soldiers_by_country(ndx) {
