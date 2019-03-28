f513ea2d69583728480622a76432516f55f3be6e
Jorden <jordenkv@gmail.com>
2019-03-28 15:34:10 +0000

    first row chart added (pokemon types)

--- static/js/graph1.js
+++ static/js/graph1.js
@@ -5,7 +5,20 @@ queue()
 function makeGraphs(error, pokedata) {
 
     var ndx = crossfilter(pokedata);
-
+    show_pokemon_type(ndx);
     dc.renderAll();
+}
+
+function show_pokemon_type(ndx) {
+    var typeDim = ndx.dimension(dc.pluck('type1'));
+    var typeGroup = typeDim.group();
+    dc.rowChart('#pokemon-type')
+        .width(900)
+        .height(400)
+        .margins({ top: 10, right: 20, bottom: 40, left: 20 })
+        .dimension(typeDim)
+        .group(typeGroup)
+        .transitionDuration(500)
+        .transitionDuration(500);
 }
 
